6ff5db4343d9f039d6cdb63d43835d1021bde16b
Richo_Han <Richo_Han@asus.com>
2016-12-21 16:27:23 +0800

    Add permission requests.

--- app/src/main/java/com/example/richo/permissionpractice/MainActivity.java
+++ app/src/main/java/com/example/richo/permissionpractice/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.richo.permissionpractice;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    private static final int MY_PERMISSIONS_REQUEST_STORAGE = 2;
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 3;
+    private FloatingActionMenu mMenu;
     private FloatingActionButton cameraActionButton, fileActionButton;
 
     @Override
@@ -15,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mMenu = (FloatingActionMenu) findViewById(R.id.menu);
         cameraActionButton = (FloatingActionButton) findViewById(R.id.camera_item);
         fileActionButton = (FloatingActionButton) findViewById(R.id.file_item);
 
@@ -27,14 +37,67 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.camera_item:
-                    Log.d("Tag", "Camera button pressed!");
+                    requestCameraPermission();
                     break;
                 case R.id.file_item:
-                    Log.d("Tag", "File button pressed!");
+                    requestStoragePermission();
                     break;
                 default:
                     break;
             }
+
+            mMenu.close(true);
         }
     };
+
+    private void requestCameraPermission() {
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                Log.d("Tag", "Explanation needed!");
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+            }
+        }
+    }
+
+    private void requestStoragePermission() {
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Log.d("Tag", "Explanation needed!");
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_STORAGE);
+            }
+        }
+    }
+
+    private void requestLocationPermission() {
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                Log.d("Tag", "Explanation needed!");
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        }
+    }
 }
