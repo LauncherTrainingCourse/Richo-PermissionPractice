c4cfec31a3593c8e71303cdf066f05ba8682c5b4
Richo_Han <Richo_Han@asus.com>
2016-12-23 14:22:44 +0800

    Implement camera features.

--- app/src/main/java/com/example/richo/permissionpractice/CameraActivity.java
+++ app/src/main/java/com/example/richo/permissionpractice/CameraActivity.java
@@ -1,13 +1,43 @@
 package com.example.richo.permissionpractice;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class CameraActivity extends Activity {
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        mCamera = getCameraInstance();
+
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return c;
     }
 }
