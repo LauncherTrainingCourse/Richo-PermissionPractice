1c5ced76ae2ca1ed43558e0d4adad702db5a66a3
Richo_Han <Richo_Han@asus.com>
2016-12-23 15:37:03 +0800

    Update layouts.

--- app/src/main/java/com/example/richo/permissionpractice/LocalPhotosFragment.java
+++ app/src/main/java/com/example/richo/permissionpractice/LocalPhotosFragment.java
@@ -1,11 +1,15 @@
 package com.example.richo.permissionpractice;
 
 
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -30,7 +34,14 @@ public class LocalPhotosFragment extends Fragment {
         ArrayList<String> photos = new ArrayList<>();
         mAdapter = new PhotoAdapter(getContext(), photos);
 
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+
         GridView gridView = (GridView) view.findViewById(R.id.photos_view);
+        gridView.setColumnWidth(width/3);
         gridView.setAdapter(mAdapter);
 
         return view;
--- app/src/main/java/com/example/richo/permissionpractice/PhotoAdapter.java
+++ app/src/main/java/com/example/richo/permissionpractice/PhotoAdapter.java
@@ -1,10 +1,13 @@
 package com.example.richo.permissionpractice;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.net.Uri;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 
 import com.facebook.drawee.view.SimpleDraweeView;
@@ -28,8 +31,18 @@ public class PhotoAdapter extends ArrayAdapter<String> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
         }
 
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+
         Uri uri = Uri.parse(url);
         SimpleDraweeView imageView = (SimpleDraweeView) convertView.findViewById(R.id.thumbnail);
+        ViewGroup.LayoutParams params = imageView.getLayoutParams();
+        params.width = width/3-4;
+        params.height = width/3-4;
+        imageView.setLayoutParams(params);
         imageView.setImageURI(uri);
 
         return convertView;
